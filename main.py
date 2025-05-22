4c2d28bff4c2d049ec40c7cf216fa68ec9900513
Gurmeet <149059595+Gurmeet788@users.noreply.github.com>
2025-05-23 02:39:14 +0500

    Update main.py
    add covid data function from api.py and set as commend mean when it click the fetch data button it call the function
    
    what is different b/w covid_data and covid_data()?
    
    if we set commend = covid-data() it show data with out clicking the button b/c it call the function directly
    
    when we use commend = covid_data it point the function when we cilck the button than it call the function ok.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from api import covid_data 
+
 root = tk.Tk()
 root.title("Covid 19 tracker")
 root.geometry("15000x15000")
@@ -7,7 +9,7 @@ root.geometry("15000x15000")
 label = tk.Label(root,text = "Welcome to Covid 19 Tracker",font=("Arial",14))
 label.pack(pady = 20)
 
-fetch_butt = tk.Button(root,text = "Fetch Data",command='api.py')
+fetch_butt = tk.Button(root,text = "Fetch Data",command= covid_data)
 fetch_butt.pack(pady = 20)
 
 exit_butt = tk.Button(root, text = "Exit", command = root.destroy)
