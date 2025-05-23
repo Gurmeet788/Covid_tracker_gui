002f823e5f75917f83f2cc7dd89a4e9603a62cca
Gurmeet <149059595+Gurmeet788@users.noreply.github.com>
2025-05-23 20:00:01 +0500

    coonect with database
    Add data base mysql  and make a database named covid_traker into locally through mysql work banch 8 and create table in database named daily_satus with atribute like recovered_caase total_case deth country e.t.c

---
+++ db_connect.py
@@ -0,0 +1,17 @@
+import mysql.connector as sql
+
+def get_connection():
+    try:
+        connection = sql.connect(
+            host="localhost",
+            user="root",     
+            password="root",
+            database="covid_tracker"
+        )
+        return connection
+    except sql.Error as err:
+        print("❌ Connection error:", err)
+        return None
+    finally:
+        if 'connection' in locals() and connection.is_connected():
+            connection.close()
