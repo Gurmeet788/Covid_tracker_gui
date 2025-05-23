3c50dd1e2b0fa9faed80ce10f8acd18a3b51cbc6
Gurmeet <149059595+Gurmeet788@users.noreply.github.com>
2025-05-23 20:03:48 +0500

    Add country list in gui.
    Ass country GUI window and select the country and fetch selected country data and show popup message and add background in window and change font and background

--- main.py
+++ main.py
@@ -1,18 +1,55 @@
 import tkinter as tk
-from api import covid_data 
+from api import country_covid_data 
+from tkinter import ttk
+
+
 
 root = tk.Tk()
 root.title("Covid 19 tracker")
 root.geometry("15000x15000")
+root.configure(background="lightblue")
 
-
-label = tk.Label(root,text = "Welcome to Covid 19 Tracker",font=("Arial",14))
+label = tk.Label(root,text = "Welcome to Covid 19 Tracker",font=("Arial Black",14),background="red")
 label.pack(pady = 20)
 
-fetch_butt = tk.Button(root,text = "Fetch Data",command= covid_data)
+ttk.Label(root,text="Country Names",font=("Arial Black",14),background="black",foreground ="white").pack(pady=20)
+select_country = tk.StringVar()
+select_country.set("Select an option") 
+country = ttk.Combobox(root,width=27,textvariable=select_country)
+country['values'] = (
+    "Pakistan",
+    "USA",
+    "Brazil",
+    "Russia",
+    "France",
+    "UK",
+    "Germany",
+    "Italy",
+    "Canada",
+    "India",
+    "Bangladesh",
+    "Australia",
+    "China",
+    "Japan",
+    "South Korea",
+    "Mexico",
+    "South Africa",
+    "Indonesia",
+    "Argentina",
+    "Spain"
+)
+country.pack(pady = 10)
+country.current()
+
+def current_country():
+    country_covid_data(select_country.get())
+
+fetch_butt = tk.Button(root,text = "Fetch Data",command = current_country)
 fetch_butt.pack(pady = 20)
+
 
 exit_butt = tk.Button(root, text = "Exit", command = root.destroy)
 exit_butt.pack(pady = 20)
+
 
 root.mainloop()
