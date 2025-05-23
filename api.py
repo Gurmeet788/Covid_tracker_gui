a01fa9d3230c8f2fb0853018d4d00034b698ebad
Gurmeet <149059595+Gurmeet788@users.noreply.github.com>
2025-05-23 19:56:43 +0500

    Add country api
    Update country wise Api and global to country name when the out popup and change the function name.

--- api.py
+++ api.py
@@ -2,15 +2,15 @@ import requests as req
 import pandas as pd
 from tkinter import messagebox
 
-def covid_data():
-    url = "https://disease.sh/v3/covid-19/all"
+def country_covid_data(country):
+    url = f"https://disease.sh/v3/covid-19/countries/{country}"
     try:
         response = req.get(url)
         if response.status_code == 200:
             data = response.json()
             df = pd.DataFrame([data])
             result = df[["cases","deaths","recovered"]].to_string(index=False)
-            messagebox.showinfo("🌍 Global COVID-19 Stats", result)
+            messagebox.showinfo( f"{country} COVID-19 Stats", result)
         else:
             messagebox.showerror("Falid to Fetch data error:",response)
     except Exception as e:
