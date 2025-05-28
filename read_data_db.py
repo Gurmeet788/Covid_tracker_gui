380defac7be0b653f11906bb570923336143ab23
Gurmeet <149059595+Gurmeet788@users.noreply.github.com>
2025-05-28 21:05:43 +0500

    read from data base
    this function return data from data base using pandas function pd.read_sql_query(quary,con) quary take what we want to perform quary and con mean connection it use sqlalchemy pak

---
+++ read_data_db.py
@@ -0,0 +1,7 @@
+import pandas as pd
+def read_data(quary,con):
+    try:
+        data = pd.read_sql_query(quary,con)
+        return data
+    except Exception as e:
+        print("conot read the data")
