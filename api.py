de6c1258d860c31d170026249d216396413ebf6b
Gurmeet <149059595+Gurmeet788@users.noreply.github.com>
2025-05-28 21:28:19 +0500

    Add  insert_data function
    when we cilck fetch button it will show message box in GUI and than it will insert into data base through insert_data()

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 import requests as req
 import pandas as pd
 from tkinter import messagebox
-
+from insert_db import insert_data
 def country_covid_data(country):
     url = f"https://disease.sh/v3/covid-19/countries/{country}"
     try:
@@ -11,6 +11,11 @@ def country_covid_data(country):
             df = pd.DataFrame([data])
             result = df[["cases","deaths","recovered"]].to_string(index=False)
             messagebox.showinfo( f"{country} COVID-19 Stats", result)
+            print("Befor sql")
+            total_cases = int(data["cases"])
+            total_deaths = int(data["deaths"])
+            total_recovered = int(data["recovered"])
+            insert_data(country, total_cases, total_deaths, total_recovered)
         else:
             messagebox.showerror("Falid to Fetch data error:",response)
     except Exception as e:
