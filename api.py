ec2bb5933d4009047510b48b62c81b79b827ad91
Gurmeet <149059595+Gurmeet788@users.noreply.github.com>
2025-05-31 00:16:31 +0500

    Fix expection which may happened
     if country == "Select an option" through warning message in popup in GUI window and change title of message box and add message through str() function.

--- api.py
+++ api.py
@@ -3,21 +3,24 @@ import pandas as pd
 from tkinter import messagebox
 from insert_db import insert_data
 def country_covid_data(country):
-    url = f"https://disease.sh/v3/covid-19/countries/{country}"
-    try:
-        response = req.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            df = pd.DataFrame([data])
-            result = df[["cases","deaths","recovered"]].to_string(index=False)
-            messagebox.showinfo( f"{country} COVID-19 Stats", result)
-            print("Befor sql")
-            total_cases = int(data["cases"])
-            total_deaths = int(data["deaths"])
-            total_recovered = int(data["recovered"])
-            insert_data(country, total_cases, total_deaths, total_recovered)
-        else:
-            messagebox.showerror("Falid to Fetch data error:",response)
-    except Exception as e:
-            messagebox.showerror("Exception occurred:", e)
-            return None
+    if country == "Select an option":
+         messagebox.showwarning("warning",str("Please Select country first"))
+    else:
+         url = f"https://disease.sh/v3/covid-19/countries/{country}"
+         try:
+               response = req.get(url)
+               if response.status_code == 200:
+                    data = response.json()
+                    df = pd.DataFrame([data])
+                    result = df[["cases","deaths","recovered"]].to_string(index=False)
+                    messagebox.showinfo( f"{country} COVID-19 Stats", result)
+                    print("Befor sql")
+                    total_cases = int(data["cases"])
+                    total_deaths = int(data["deaths"])
+                    total_recovered = int(data["recovered"])
+                    insert_data(country, total_cases, total_deaths, total_recovered)
+               else:
+                    messagebox.showerror("Error",str("Falid to Fetch data error:",response))
+         except Exception as e:
+              messagebox.showerror("Exception occurred:", str(e))
+              return None
