572919af116187a0ad693a331f9f4ac5b6c3ab83
Gurmeet <149059595+Gurmeet788@users.noreply.github.com>
2025-05-31 00:21:29 +0500

    Add visualization button and background picture
    Add visualization button and background picture ,when i cilck the button it will show ploat of selected country, which save in my sql with date and case of country.

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import tkinter as tk
 from api import country_covid_data 
 from tkinter import ttk
-
-
+from visualization import visualization
+from PIL import Image,ImageTk
 
 root = tk.Tk()
 root.title("Covid 19 tracker")
-root.geometry("15000x15000")
-root.configure(background="lightblue")
+root.geometry("600x442")
 
+image = Image.open("covid_19.jpg")
+image = image.resize((600,442),Image.Resampling.LANCZOS)
+covid_image = ImageTk.PhotoImage(image)
+
+image_label = tk.Label(root, image=covid_image)
+image_label.place(x=0, y=0, relwidth=1, relheight=1)
 label = tk.Label(root,text = "Welcome to Covid 19 Tracker",font=("Arial Black",14),background="red")
 label.pack(pady = 20)
 
@@ -16,7 +21,7 @@ ttk.Label(root,text="Country Names",font=("Arial Black",14),background="black",f
 select_country = tk.StringVar()
 select_country.set("Select an option") 
 country = ttk.Combobox(root,width=27,textvariable=select_country)
-country['values'] = (
+country['values'] = ( 
     "Pakistan",
     "USA",
     "Brazil",
@@ -44,9 +49,15 @@ country.current()
 def current_country():
     country_covid_data(select_country.get())
 
+def vil():
+    visualization(select_country.get())
+
+
 fetch_butt = tk.Button(root,text = "Fetch Data",command = current_country)
 fetch_butt.pack(pady = 20)
 
+visualization_butt = tk.Button(root,text="Visualization",command = vil)
+visualization_butt.pack(pady=20)
 
 exit_butt = tk.Button(root, text = "Exit", command = root.destroy)
 exit_butt.pack(pady = 20)
